a395ee46525a02051ce84d9ffbe7b476a65f159c
Yoel <yoelb2006@gmail.com>
2023-11-03 11:51:35 +0200

    threeactivities

--- app/src/main/java/com/example/emssioncalculator/MainActivity.java
+++ app/src/main/java/com/example/emssioncalculator/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.emssioncalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogIn;
+    private TextView tvSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvSignIn = findViewById(R.id.tvSignUp);
+        btnLogIn = findViewById(R.id.btnLogin);
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
