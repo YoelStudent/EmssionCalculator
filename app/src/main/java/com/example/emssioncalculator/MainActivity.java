4d3edac07deaceac0bda53edb666aa290253a5f6
Yoel <yoelb2006@gmail.com>
2023-11-23 10:07:46 +0200

    database and checking email

--- app/src/main/java/com/example/emssioncalculator/MainActivity.java
+++ app/src/main/java/com/example/emssioncalculator/MainActivity.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnLogIn;
     private TextView tvSignIn;
+    private EditText edEmail, edPass;
     private MyDatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvSignIn = findViewById(R.id.tvSignUp);
         btnLogIn = findViewById(R.id.btnLogin);
+        edPass = findViewById(R.id.editTextPassword);
+        edEmail = findViewById(R.id.editTextUsername);
         db = new MyDatabaseHelper(this);
         tvSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MainPageActivity.class);
-                startActivity(intent);
+                String Email = edEmail.getText().toString();
+                String Pass = edPass.getText().toString();
+                Boolean logInResult = db.CheckLogIn(Email, Pass);
+                if (logInResult)
+                {
+                    Intent intent = new Intent(MainActivity.this, MainPageActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "failed login", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
