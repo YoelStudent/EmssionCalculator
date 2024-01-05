0a65ad0b07f778432cda348e6e4b4c46271e766d
Yoel <yoelb2006@gmail.com>
2024-01-05 15:07:59 +0200

    sharedpref works and some frags

--- app/src/main/java/com/example/emssioncalculator/MainPageActivity.java
+++ app/src/main/java/com/example/emssioncalculator/MainPageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,32 +14,35 @@ public class MainPageActivity extends AppCompatActivity
 {
     private Boolean flag = true;
     ImageButton btn_add;
+    ImageButton btn_profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
         btn_add = findViewById(R.id.btnCalculateRoute);
+        btn_profile = findViewById(R.id.btnAccount);
+
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 FragmentManager fragmentManager = getSupportFragmentManager();
-
-                if (flag)
-                {
-                    fragmentManager.beginTransaction().replace(R.id.fragment_container_view, NewCalc.class, null).commit();
-                    flag = !flag;
-                }
-                else
-                {
-                    Fragment newCalc = fragmentManager.findFragmentById(R.id.fragment_container_view);
-                    fragmentManager.beginTransaction()
-                            .remove(newCalc)
-                            .commit();
-                    flag = !flag;
-                }
-
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, NewCalc.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, ProfileFrag.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
             }
         });
 
